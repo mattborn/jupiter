62d1831326d3bb5ee7bb36ed748da4b0448a5941
Matt Born <mattborn@users.noreply.github.com>
2019-11-18 17:17:37 -0700

    Add username, files, and collapse sections

--- scripts.js
+++ scripts.js
@@ -1,12 +1,15 @@
-const PERF = 10000 // base performance unit as max milliseconds
+const PERF = 100000 // base performance unit as max milliseconds
 
-let state = JSON.parse(localStorage.getItem('fakeZeus')) || {
+let state = JSON.parse(localStorage.getItem('jupiter')) || {
+  integrations: {},
   jobs: {},
   tab: 0,
 }
+state.name = state.name || prompt('Please enter your name that others can see in Zeus', '')
+document.querySelectorAll('#Zeus-Head a:last-child').forEach(el => el.innerHTML += state.name)
 
 function stateChanged() {
-  localStorage.setItem('fakeZeus', JSON.stringify(state))
+  localStorage.setItem('jupiter', JSON.stringify(state))
   render()
 }
 
@@ -17,13 +20,15 @@ function render() {
 
 function getFiles() {
   let files = []
-  const numFiles = Math.ceil(Math.random() * 5) // up to 5 files
+  const numFiles = 5 /*Math.ceil(Math.random() * 5)*/ // up to 5 files
   for (let i = 0; i < numFiles; i++) {
     let file = {}
+    file.lander = 'lander_1234…'
     file.name = cuid()
-    file.landed = (Math.random() < 0.05)
+    file.landed = (Math.random() > 0.2)
     files.push(file)
   }
+  console.log(files)
   return files
 }
 
@@ -35,12 +40,19 @@ function getQualityStatus() {
   return (Math.random() < 0.5) ? 'All quality checks passed' : 'Quality checks failed'
 }
 
+// function makeIntegration() {
+//   return {
+//     'hooli_vendo_eligibility'
+//   }
+// }
+
 function runJob() {
   let job = {}
   job.created = Date.now()
+  job.by = state.name
   job.id = parseInt(job.created.toString().substr(-6))
-  job.eta = Math.floor(job.created + (Math.random() * (PERF * 2)))
-  job.files = getFiles()
+  job.eta = (getJobs().length < 1) ? 'No prior jobs' : Math.floor(job.created + (Math.random() * (PERF * 2)))
+  // job.files = getFiles()
   job.integration = 'hooli_vendo_eligibility'
   job.status = 'Running job on Databricks…'
   state.jobs[job.id] = job
@@ -119,6 +131,31 @@ const gridOptions_Activity = {
 }
 new agGrid.Grid(document.querySelector('#Zeus-Activity'), gridOptions_Activity)
 
+const gridOptions_Files = {
+  defaultColDef: {
+    enableRowGroup: true,
+    enablePivot: true,
+    filter: 'agTextColumnFilter',
+    resizable: true,
+    sortable: true,
+  },
+  domLayout: 'autoHeight',
+  floatingFilter: true,
+  columnDefs: [
+    {headerName: 'Lander', field: 'lander'},
+    {headerName: 'Filename', field: 'name'},
+    {headerName: 'Landed', field: 'landed'},
+    {headerName: 'Actions', cellRenderer: () => '<a class="far fa-eye" href="#"></a>'}
+  ],
+  rowClassRules: {
+    'fail': (params) => !params.data.landed
+  },
+  rowData: getFiles(),
+  onFirstDataRendered: (params) => params.api.sizeColumnsToFit(),
+  onGridReady: autoSizeActivity,
+}
+new agGrid.Grid(document.querySelector('#Zeus-Files'), gridOptions_Files)
+
 function linkToDatabricks(params) {
   return '<a href="#" title="View on Databricks">'+ params.value +'</a>'
 }
@@ -128,7 +165,8 @@ function formatTime(params) {
 }
 
 function formatETA(params) {
-  return (state.jobs.length < 1) ? 'No prior jobs' : moment(params.value).fromNow() +' · '+ moment(params.value).format('LT')
+  const val = params.value
+  return typeof val === 'string' ? val : moment(val).fromNow() +' · '+ moment(val).format('LT')
 }
 
 function autoSizeJobs() {
@@ -152,10 +190,13 @@ document.getElementById('Zeus-Run-Integration').addEventListener('click', (e) =>
   runJob()
 })
 
+document.querySelectorAll('.Section-Head').forEach(el =>
+  el.addEventListener('click', (e) => el.parentElement.classList.toggle('expand')))
+
 render()
 
 document.addEventListener('keydown', (e) => {
-  console.log(e.which)
+  // console.log(e.which)
   if (e.which === 49) {
     document.getElementById('Zeus-Run-Integration').dispatchEvent(new Event('click'))
   }
